2e1784b8f874457d7b668397c0f31b1f791ef838
Lucas M <lucasmmayall@gmail.com>
2023-05-12 18:42:02 -0700

    Making responsive and close when clicking away

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -4,10 +4,11 @@ import SocialButton from "./SocialButton";
 import profilePhoto from "../../public/profile-photo.jpg";
 import Image from "next/image";
 import { Github_icon, LinkedIn_icon, Mail_icon } from "./SvgIcons";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 export function NavigationMenu() {
   const [menuVisible, setMenuVisible] = React.useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const pages: { name: string; link: string }[] = [
     { name: "Home", link: "/" },
@@ -20,17 +21,56 @@ export function NavigationMenu() {
     setMenuVisible(!menuVisible);
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    const boundHandleClickOutside = handleClickOutside.bind(null);
+    document.addEventListener("mousedown", boundHandleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", boundHandleClickOutside);
+    };
+  }, []);
+
   return (
-    <div>
-      <div className="bg-red-500">
-        <button
-          className="bg-blue-500 fixed left-0 top-0 z-20 p-4 text-secondary focus:outline-none w-full"
-          onClick={toggleMenu}
-        >
-          <div className="h-1 w-6 mb-1 bg-primary"></div>
-          <div className="h-1 w-6 mb-1 bg-primary"></div>
-          <div className="h-1 w-6 bg-primary"></div>
-        </button>
+    <div ref={menuRef}>
+      <div className="">
+        <div className={`fixed left-0 top-0 z-20 bg-accent`}>
+          <button
+            className="bg-accent p-4 text-secondary focus:outline-none"
+            onClick={toggleMenu}
+          >
+            <div
+              className={`h-1 w-6 mb-1 bg-primary transition-transform duration-300 ease-in-out ${
+                menuVisible ? "transform -rotate-45 translate-y-2" : ""
+              }`}
+            ></div>
+            <div
+              className={`h-1 w-6 mb-1 bg-primary transition-opacity duration-300 ease-in-out ${
+                menuVisible ? "opacity-0" : "opacity-100"
+              }`}
+            ></div>
+            <div
+              className={`h-1 w-6 bg-primary transition-transform duration-300 ease-in-out ${
+                menuVisible ? "transform rotate-45 -translate-y-2" : ""
+              }`}
+            ></div>
+          </button>
+        </div>
+
+        {/* <div className={`fixed left-0 top-0 z-20 bg-accent`}>
+          <button
+            className="bg-accent  p-4 text-secondary focus:outline-none"
+            onClick={toggleMenu}
+          >
+            <div className="h-1 w-6 mb-1 bg-primary"></div>
+            <div className="h-1 w-6 mb-1 bg-primary"></div>
+            <div className="h-1 w-6 bg-primary"></div>
+          </button>
+        </div> */}
 
         <nav
           className={`pt-20 fixed h-full left-0 top-0 h-screen w-64 bg-secondary text-primary flex flex-col p-6 justify-between z-10 transform transition-transform duration-300 ease-in-out ${
@@ -49,7 +89,7 @@ export function NavigationMenu() {
 
             <ul className="text-lg py-0">
               {pages.map((item) => (
-                <li>
+                <li onClick={() => setMenuVisible(false)} key={item.name}>
                   <Link
                     href={item.link}
                     className={
