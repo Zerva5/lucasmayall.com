c88fd0ebd074f7451361d7856b1d3a283653a843
Lucas M <lucasmmayall@gmail.com>
2023-05-13 18:54:43 -0700

    Added bottom accent border to techlist content

--- src/components/TechList.tsx
+++ src/components/TechList.tsx
@@ -5,7 +5,7 @@ export default function TechList({ technologies }: { technologies: string[] }) {
   technologies.sort((a, b) => a.localeCompare(b));
 
   return (
-    <div className="tech-icons flex flex-column flex-wrap">
+    <div className="tech-icons flex flex-column flex-wrap  border-accent border-b-4 p-2 ml-2">
       {technologies.map((tech, index) => (
         <div
           className="tech-icon-container my-auto mx-1 h-8 w-8 flex "
