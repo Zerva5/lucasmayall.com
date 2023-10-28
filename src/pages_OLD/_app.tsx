502fd17fcc1cff950834e32d9b5e6f4a983c55da
Lucas Mayall <lucasmmayall@gmail.com>
2023-10-27 21:53:41 -0600

    Delete old pages

--- src/pages_OLD/_app.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
- 
-export default function MyApp({ Component, pageProps }: any) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
