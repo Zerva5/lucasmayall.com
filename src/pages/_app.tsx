b8128ca8c7a99140d66ec45456a0166b568a899d
Lucas Mayall <lucasmmayall@gmail.com>
2023-10-27 22:05:35 -0600

    pls fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,6 @@ import Link from "next/link";
 export default function MyApp({ Component, pageProps }: any) {
   return (
     <>
-    <Head>
-      <Link rel="preconnect" href="https://fonts.googleapis.com"></Link>
-      <Link rel="preconnect" href="https://fonts.gstatic.com" ></Link>
-      <Link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap" rel="stylesheet"></Link>
-    </Head>
     {/* </Head> */} 
       <Component {...pageProps} />
       </>
